ae88efb019348b70e3ad680414e330629b9d3acf
linhao <linhao175@qq.com>
2020-06-13 01:20:17 +0800

    do data work

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -6,44 +6,55 @@ import unittest
 # 导入自己的模块需要添加系统路径
 import sys
 sys.path.append(os.path.abspath('.'))
-from utils.config import Config, DRIVER_PATH
+from utils.config import Config, DRIVER_PATH, DATA_PATH
 from utils.log import logger
+from utils.file_reader import ExcelReader
 
 class TestBaiDu(unittest.TestCase):
-    # 配置分离
+    # 配置分离 数据分离
     URL = Config().get('URL')
+    excel = DATA_PATH + '/baidu.xlsx'
 
     locator_kw = (By.ID, 'kw')
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//*[@id="content_left"]/div/h3/a')
     
-    def setUp(self):
+    def sub_setUp(self):
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + r'\chromedriver.exe')
         self.driver.get(self.URL)
     
-    def tearDown(self):
+    def sub_tearDown(self):
         self.driver.quit()
 
-    def test_search_0(self):
-        self.driver.find_element(*self.locator_kw).send_keys("selenium")
-        self.driver.find_element(*self.locator_su).click()
-        sleep(2)
-        results = self.driver.find_elements(*self.locator_result)
-        for r in results:
-            logger.info(r.get_attribute('href'))
-            logger.info(r.text)
+    def test_search(self):
+        datas = ExcelReader(self.excel).data
+        for d in datas:
+            with self.subTest(data=d):
+                logger.debug("test begin -> %s" % d['search'])
+                self.sub_setUp()
+                self.driver.find_element(*self.locator_kw).send_keys(d['search'])
+                self.driver.find_element(*self.locator_su).click()
+                sleep(2)  # 不sleep下面的results可能会获取不到元素
 
-    def test_search_1(self):
-        self.driver.find_element(*self.locator_kw).send_keys("python")
-        self.driver.find_element(*self.locator_su).click()
-        sleep(2)
-        results = self.driver.find_elements(*self.locator_result)
-        for r in results:
-            logger.info(r.get_attribute('href'))
-            logger.info(r.text)
+                results = self.driver.find_elements(*self.locator_result)
+                logger.debug("results:{}".format(results))
+                for r in results:
+                    logger.info(r.get_attribute('href'))
+                    logger.info(r.text)
+                
+                self.sub_tearDown()
+                logger.debug("test end -> %s" % d['search'])
+
+
+
+
 
 if __name__ == "__main__":
-    unittest.main()
+#     verbosity是一个选项,表示测试结果的信息复杂度，有0、1、2 三个值
+# 0 (静默模式): 你只能获得总的测试用例数和总的结果 比如 总共10个 失败2 成功8
+# 1 (默认模式): 非常类似静默模式 只是在每个成功的用例前面有个“.” 每个失败的用例前面有个 “F”
+# 2 (详细模式):测试结果会显示每个测试用例的所有相关的信息
+    unittest.main(verbosity=2)
 
 
 # 通过By和直接调用无差别
--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+from xlrd import open_workbook # excel
 
 # 初始化一个YamlReader传入yml文件路径，用data函数获取load之后的list数据
 class YamlReader:
@@ -20,13 +21,85 @@ class YamlReader:
                 self._data = list(yaml.safe_load_all(f))
         return self._data
 
+# 自定义异常类
+class SheetTypeError(Exception):
+    pass
+
+class ExcelReader:
+    """
+    读取excel文件中的内容。返回list。
+
+    如：
+    excel中内容为：
+    | A  | B  | C  |
+    | A1 | B1 | C1 |
+    | A2 | B2 | C2 |
+
+    如果 print(ExcelReader(excel, title_line=True).data)，输出结果：
+    [{A: A1, B: B1, C:C1}, {A:A2, B:B2, C:C2}]
+
+    如果 print(ExcelReader(excel, title_line=False).data)，输出结果：
+    [[A,B,C], [A1,B1,C1], [A2,B2,C2]]
+
+    可以指定sheet，通过index或者name：
+    ExcelReader(excel, sheet=2)
+    ExcelReader(excel, sheet='BaiDuTest')
+    """
+    def __init__(self, excel, sheet = 0, title_line = True):
+        if not os.path.exists(excel):
+            raise FileNotFoundError("excel file not found, path: %s" % excel)
+        else:
+            self.excel = excel
+        self.sheet = sheet
+        self.title_line = title_line
+        self._data = list()  # 数据初始化为一个list
+
+    @property
+    def data(self):
+        # data为空list则获取数据
+        if not self._data:
+            # 打开data excel
+            workbook = open_workbook(self.excel)
+            
+            # 如果sheet名为数字则获取table_by_index
+            # str则sheet_by_name
+            if type(self.sheet) not in [int, str]:
+                raise ValueError("excelReader.sheet must be int or str, but now is %s" % type(self.sheet))
+            elif type(self.sheet) == int:
+                s = workbook.sheet_by_index(self.sheet)
+            elif type(self.sheet) == str:
+                s = workbook.sheet_by_name(self.sheet)
+            
+            # 如果有标题栏
+            if self.title_line:
+                title = s.row_values(0)  # 首行为title
+                for col in range(1, s.nrows):
+                    # 通过zip函数将title和行值组成tuple再转为dict,用于可迭代对象
+                    # dict(zip(iterable, iterable))常见用法
+                    self._data.append(dict(zip(title, s.row_values(col))))
+            else:
+                # 无标题则直接添加至list[[v00, v01, v02], [v10, v11, v12]]
+                # 相当于二维数组
+                for col in range(s.nrows):
+                    self._data.append(s.row_values(col))  # row_values(index) 返回一行的list
+        return self._data
 
 
-if __name__ == "__main__":
+
+# TEST
+def test_yamlReader():
     run_path = os.path.abspath('.')
     config_path = os.path.join(run_path, 'config/config.yml')
-
     reader = YamlReader(config_path)
     print(reader.data)
 
+def test_excelReader():
+    run_path = os.path.abspath('.')
+    data_path = os.path.join(run_path, 'data/baidu.xlsx')
+    print(data_path)
+    reader = ExcelReader(data_path)
+    print(reader.data)
+    pass
+if __name__ == "__main__":
+    test_excelReader()
     
